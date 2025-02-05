120c0d01646b954707f8653cf9b836a46c110fb7
AedinJRC <aedin.cabrales@gmail.com>
2025-02-05 17:21:07 +0800

    First

---
+++ GSO.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>GSO</title>
+   <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+   <nav class="sidebar">
+      <ul>
+         <li class="active">
+            <a href="GSO.php">
+               <span class="icon"><i class="fas fa-home"></i></span>
+               <span class="title">Home</span>
+            </a>
+         </li>
+         <li>
+            <a href="GSO.php"></a>
+         </li>
+         <li>
+            <a href="GSO.php"></a>
+         </li>
+         <li>
+            <a href="GSO.php"></a>
+         </li>
+      </ul>
+   </nav>
+   <main>
+
+   </main>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,5 @@
+<html>
+   <body>
+      woweafwe
+   </body>
+</html>
