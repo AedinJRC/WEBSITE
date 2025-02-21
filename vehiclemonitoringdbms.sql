4a7e292934f290a0f219681673af7e9c7c795974
AedinJRC <aedin.cabrales@gmail.com>
2025-02-21 16:07:03 +0800

    sql - Aedin

---
+++ vehiclemonitoringdbms.sql
@@ -0,0 +1,41 @@
+/*
+SQLyog Ultimate - MySQL GUI v8.22 
+MySQL - 5.5.5-10.4.32-MariaDB : Database - vehiclemonitoringdbms
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`vehiclemonitoringdbms` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
+
+USE `vehiclemonitoringdbms`;
+
+/*Table structure for table `usertb` */
+
+DROP TABLE IF EXISTS `usertb`;
+
+CREATE TABLE `usertb` (
+  `employeeid` varchar(10) NOT NULL,
+  `ppicture` varchar(255) DEFAULT NULL,
+  `fname` varchar(30) DEFAULT NULL,
+  `lname` varchar(20) DEFAULT NULL,
+  `pword` varchar(100) DEFAULT NULL,
+  `role` varchar(20) DEFAULT 'User',
+  `created_at` datetime DEFAULT NULL,
+  PRIMARY KEY (`employeeid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+/*Data for the table `usertb` */
+
+insert  into `usertb`(`employeeid`,`ppicture`,`fname`,`lname`,`pword`,`role`,`created_at`) values ('admin',NULL,'AAMA','DVDRC','admin','Admin','2025-02-21 13:35:00');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
