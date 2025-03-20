bb1aa863d18ffc339f7599ecdac23ead67003623
YANNA1229 <delavega.alyanna_nicole@csa-binan.edu.ph>
2025-03-20 15:11:46 +0800

    20/03 - yanna

--- dept.php
+++ dept.php
@@ -1,27 +1,3 @@
-<?php
-// Database connection
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "vehiclemonitoringdbms";
-
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['employeeid'], $_POST['fname'], $_POST['lname'], $_POST['role'], $_POST['created_at'])) {
-    $employeeid = $_POST['employeeid'];
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $role = $_POST['role'];
-    $created_at = $_POST['created_at'];
-
-    $sql = "UPDATE usertb SET fname='$fname', lname='$lname', role='$role', created_at='$created_at' WHERE employeeid='$employeeid'";
-    mysqli_query($conn, $sql);
-}
-
-mysqli_close($conn);
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -84,20 +60,13 @@ mysqli_close($conn);
     <div class="container">
         <h2>Manage Accounts</h2>
         <div class="table-container">
-            <?php
-                include "config.php";
-                $selectsql = "SELECT * FROM usertb ORDER BY lname ASC";
-                $resultsql = $conn->query($selectsql);
-                
-                
-            ?>
             <table>
                 <thead>
                     <tr>
                         <th>Profile Picture</th>
                         <th>First Name</th>
                         <th>Last Name</th>
-                        <th>Role</th>
+                        <th>Department</th>
                         <th>Date Added</th>
                         <th>Actions</th>
                     </tr>
@@ -123,18 +92,6 @@ mysqli_close($conn);
                             <button class="btn delete-btn">Delete</button>
                         </td>
                     </tr>
-                        <?php
-                        while ($row = $resultsql->fetch_assoc()) 
-                        {
-                            echo "<tr>";
-                                echo "<td><img src='uploads/' alt=''></td>";
-                                echo "<td><input type='text' value='" . $row['fname'] . "'></td>";
-                                echo "<td><input type='text' value='" . $row['lname'] . "'></td>";
-                                echo "<td><input type='text' value='" . $row['role'] . "'></td>";
-                                echo "<td><input type='text' value='" . $row['created_at'] . "'></td>";
-                            echo "</tr>";
-                        }
-                        ?>
                 </tbody>
             </table>
         </div>
